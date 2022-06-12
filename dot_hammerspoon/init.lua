48bc4bd9b5a69d516891510431c7c2cf5ab56648
Greg Weber <gregwebs@users.noreply.github.com>
2022-06-12 13:57:09 -0500

    update init.lua

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -1,7 +1,18 @@
+require("hyper")
+require("console")
+-- require("mouse")
+
 -- https://www.hammerspoon.org/docs/hs.window.switcher.html
 --
 switcher = hs.window.switcher.new(hs.window.filter.new():setCurrentSpace(true):setDefaultFilter{}) -- include minimized/hidden windows, current Space only
+switcher.ui.showThumbnails = false
 
 -- bind to hotkeys; WARNING: at least one modifier key is required!
 hs.hotkey.bind('alt','escape','Next window',function()switcher:next()end)
 hs.hotkey.bind('alt-shift','escape','Prev window',function()switcher:previous()end)
+
+-- function reload_config(files)
+--    hs.reload()
+-- end
+-- hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
+hs.alert.show("Config loaded")
